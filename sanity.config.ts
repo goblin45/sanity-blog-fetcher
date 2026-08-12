61d0677feab5b35e53de9e853e5c7e2e2e17f0f6
Rajarshi Dutta <97035849+goblin45@users.noreply.github.com>
2026-08-12 10:20:30 +0530

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'Nexval Blogs',
 
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
 
   plugins: [structureTool(), visionTool()],
 
